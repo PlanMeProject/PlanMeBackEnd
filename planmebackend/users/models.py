02c02b2ce2986b0345df18a5a54c5dc24a5d0979
PhumrapeeC <phumrapeegenius@gmail.com>
2023-10-17 15:52:57 +0700

    Set up database.

--- planmebackend/users/models.py
+++ planmebackend/users/models.py
@@ -1,26 +1,31 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
-
 class User(AbstractUser):
-    """
-    Default custom user model for PlanMeBackEnd.
-    If adding fields that need to be filled at user signup,
-    check forms.SignupForm and forms.SocialSignupForms accordingly.
-    """
+    token = models.CharField(max_length=255, blank=True)
 
-    # First and last name do not cover name patterns around the globe
-    name = CharField(_("Name of User"), blank=True, max_length=255)
-    first_name = None  # type: ignore
-    last_name = None  # type: ignore
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    due_date = models.DateField()
+    status = models.CharField(max_length=255)
 
-    def get_absolute_url(self) -> str:
-        """Get URL for user's detail view.
+class SubTask(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    summarized_text = models.TextField(blank=True)
+    bullet_text = models.TextField(blank=True)
+    due_date = models.DateField()
+    status = models.CharField(max_length=255)
 
-        Returns:
-            str: URL for user detail.
+class Dashboard(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-        """
-        return reverse("users:detail", kwargs={"username": self.username})
+class DataVisualization(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    type = models.CharField(max_length=255)
