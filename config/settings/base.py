aae7ba42e1bc2dfcb04a0c95861fc6cec06a08f8
Krittin SETDHAVANICH <krittin.set@ku.th>
2023-10-19 20:55:12 +0700

    uncomment AUTH_USER

--- config/settings/base.py
+++ config/settings/base.py
@@ -104,9 +104,9 @@ AUTHENTICATION_BACKENDS = [
     "allauth.account.auth_backends.AuthenticationBackend",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-user-model
-# AUTH_USER_MODEL = "users.User"
+AUTH_USER_MODEL = "app.User"
 # https://docs.djangoproject.com/en/dev/ref/settings/#login-redirect-url
-# LOGIN_REDIRECT_URL = "users:redirect"
+LOGIN_REDIRECT_URL = "app:redirect"
 # https://docs.djangoproject.com/en/dev/ref/settings/#login-url
 LOGIN_URL = "account_login"
 
