926d516bb53665ad10606334be5ff3218b6139da
Krittin SETDHAVANICH <krittin.set@ku.th>
2023-10-19 15:18:00 +0700

    Updated model_abstracts to inherits TimeStampedModel instead of Model

--- utils/model_abstracts.py
+++ utils/model_abstracts.py
@@ -1,11 +1,12 @@
 """Abstract model classes for generate unique id for each model."""
 import uuid
 from django.db import models
+from django_extensions.db.models import TimeStampedModel
 
 
-class Model(models.Model):
-    """Abstract model class."""
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+class Model(TimeStampedModel):
+    """Abstract model class for generate unique id for each model."""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     class Meta:
         """Meta definition for Model."""
