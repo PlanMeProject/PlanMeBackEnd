c13f4435d9159460aab4d702892ec4eeafe03358
Krittin SETDHAVANICH <krittin.set@ku.th>
2023-10-19 17:05:40 +0700

    delete planmebackend/users and their path, added the planmebackend/app path

--- config/api_router.py
+++ config/api_router.py
@@ -1,14 +1,21 @@
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
-from planmebackend.users.api.views import UserViewSet
+from planmebackend.app.api.views import UserViewSet, TaskViewSet, \
+    SubTaskViewSet, DashboardViewSet, DataVisualizationViewSet
+
 
 if settings.DEBUG:
     router = DefaultRouter()
 else:
     router = SimpleRouter()
 
-router.register("users", UserViewSet)
+router.register(r'users', UserViewSet, basename='user')
+router.register(r'tasks', TaskViewSet, basename='task')
+router.register(r'subtasks', SubTaskViewSet, basename='subtask')
+router.register(r'dashboards', DashboardViewSet, basename='dashboard')
+router.register(r'data-visualizations', DataVisualizationViewSet,
+                basename='data-visualization')
 
 
 app_name = "api"
