53eb4a5088490b1ae58ed943ba0c7fc2192063c7
PhumrapeeC <phumrapeegenius@gmail.com>
2023-10-22 13:52:38 +0700

    Set up google classroom api

--- planmebackendapp/quickstart.py
+++ planmebackendapp/quickstart.py
@@ -1,52 +1,54 @@
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
-# Initialize the API
-scopes = [
-    "https://www.googleapis.com/auth/classroom.courses.readonly",
-    "https://www.googleapis.com/auth/classroom.rosters.readonly",
-    "https://www.googleapis.com/auth/classroom.course-work.readonly",
-    "https://www.googleapis.com/auth/classroom.student-submissions.me.readonly",
-]
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/classroom.courses.readonly"]
 
-flow = InstalledAppFlow.from_client_secrets_file("credentials.json", scopes)
-credentials = flow.run_local_server(port=0)
-service = build("classroom", "v1", credentials=credentials)
 
-# Get the list of courses where you are a student
-results = service.courses().list(studentId="me").execute()
-courses = results.get("courses", [])
+def main():
+    """Shows basic usage of the Classroom API.
+    Prints the names of the first 10 courses the user has access to.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
 
-# Loop through each course
-for course in courses:
-    print("Course Name:", course["name"])
+    try:
+        service = build("classroom", "v1", credentials=creds)
 
-    # Get the list of students in the course
-    students_results = service.courses().students().list(courseId=course["id"]).execute()
-    students = students_results.get("students", [])
+        # Call the Classroom API
+        results = service.courses().list(pageSize=10).execute()
+        courses = results.get("courses", [])
 
-    # Loop through each student
-    for student in students:
-        print("Student Name:", student["profile"]["name"]["fullName"])
+        if not courses:
+            print("No courses found.")
+            return
+        # Prints the names of the first 10 courses.
+        print("Courses:")
+        for course in courses:
+            print(course["name"])
 
-    # Get the list of assignments (course work) in the course
-    coursework_results = service.courses().courseWork().list(courseId=course["id"]).execute()
-    course_works = coursework_results.get("courseWork", [])
+    except HttpError as error:
+        print("An error occurred: %s" % error)
 
-    # Loop through each assignment
-    for work in course_works:
-        # Get student submissions for this assignment
-        submissions_results = (
-            service.courses()
-            .courseWork()
-            .studentSubmissions()
-            .list(courseId=course["id"], courseWorkId=work["id"], userId="me")
-            .execute()
-        )
-        submissions = submissions_results.get("studentSubmissions", [])
 
-        # Check if the assignment is not done
-        if not any(submission.get("state") in ["TURNED_IN", "RETURNED"] for submission in submissions):
-            print("Assignment Title:", work["title"])
-            print("Assignment Description:", work.get("description", "No description"))
-            print("Due Date:", work.get("dueDate", "No due date"))
+if __name__ == "__main__":
+    main()
