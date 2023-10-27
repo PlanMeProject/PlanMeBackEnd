210c168aa60e34b9821e5ffe9165c1362b312001
PhumrapeeC <phumrapeegenius@gmail.com>
2023-10-27 14:52:29 +0700

    Combine task and subtask into one function called course_information.

--- planmebackend/app/quickstart.py
+++ planmebackend/app/quickstart.py
@@ -45,7 +45,8 @@ class GoogleClassroomAPI:
             student_info.append(info)
         return student_info
 
-    def task(self, course):
+    def course_information(self, course):
+        # Course Data
         course_data = {
             "course_id": course["id"],
             "course_name": course["name"],
@@ -53,48 +54,44 @@ class GoogleClassroomAPI:
             "course_description": course.get("description"),
             "course_status": course.get("courseState"),
         }
-        return course_data
 
-    def subtask(self, course_id):
-        assignments = self.get_works(course_id).get("courseWork", [])
-
+        # Assignments Information
+        assignments = self.get_works(course_data["course_id"]).get("courseWork", [])
         assignments_info = []
+
         for assignment in assignments:
-            # Get student submissions for this assignment
             submissions_results = (
                 self.service.courses()
                 .courseWork()
                 .studentSubmissions()
-                .list(courseId=course_id, courseWorkId=assignment["id"], userId="me")
+                .list(courseId=course_data["course_id"], courseWorkId=assignment["id"], userId="me")
                 .execute()
             )
             submissions = submissions_results.get("studentSubmissions", [])
 
-            # Check if the assignment is not done
             if not any(submission.get("state") in ["TURNED_IN", "RETURNED"] for submission in submissions):
                 info = {
-                    "course_id": course_id,
+                    "course_id": course_data["course_id"],
                     "title": assignment.get("title"),
                     "description": assignment.get("description"),
-                    "due_date": str(assignment.get("dueDate", {}).get("year"))
-                    + "-"
-                    + str(assignment.get("dueDate", {}).get("month"))
-                    + "-"
-                    + str(assignment.get("dueDate", {}).get("day")),
+                    "due_date": "{}-{}-{}".format(
+                        assignment.get("dueDate", {}).get("year"),
+                        assignment.get("dueDate", {}).get("month"),
+                        assignment.get("dueDate", {}).get("day"),
+                    ),
                     "status": assignment.get("state"),
                     "max_points": assignment.get("maxPoints"),
                 }
                 assignments_info.append(info)
-        return assignments_info
 
-    def main(self):
-        courses = self.get_courses()
-        for course in courses:
-            print("Course Information:", self.task(course))
-            print("Assignments Information:", self.subtask(course["id"]))
-            print()
+        return course_data, assignments_info
 
 
 if __name__ == "__main__":
     api = GoogleClassroomAPI()
-    api.main()
+    courses = api.get_courses()
+    for course in courses:
+        course_info, assignments_info = api.course_information(course)
+        print("Course Information:", course_info)
+        print("Assignments Information:", assignments_info)
+        print()
