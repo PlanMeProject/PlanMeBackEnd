088ffc9c5f8e6f650608d68688ef76f9aad010be
Krittin SETDHAVANICH <krittin.set@ku.th>
2023-11-04 14:25:12 +0700

    added nlp_url'

--- planmebackend/utils/setupTest.py
+++ planmebackend/utils/setupTest.py
@@ -22,6 +22,7 @@ class BaseTestCase(APITestCase):
         self.user_url = "/api/users/"
         self.task_url = f"/api/users/{self.user.id}/tasks/"
         self.subtask_url = f"/api/users/{self.user.id}/tasks/{self.task.id}/subtasks/"
+        self.nlp_url = "/api/nlp/"
         self.token, created = Token.objects.get_or_create(user=self.user)
         self.client = APIClient()
         self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token.key)
