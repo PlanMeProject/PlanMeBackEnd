384d2955e2e62178958d9d5c9d8045f1b2669e19
Krittin SETDHAVANICH <krittin.set@ku.th>
2023-11-05 10:22:08 +0700

    Added SummarizeViewSet

--- planmebackend/nlp/views/summarize_view.py
+++ planmebackend/nlp/views/summarize_view.py
@@ -1,24 +1,36 @@
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import status, viewsets
 from rest_framework.response import Response
 
 from planmebackend.app.models import Task
-from planmebackend.app.serializers import SubTaskSerializer
+from planmebackend.app.serializers import TaskSerializer
 from planmebackend.nlp.apps import NlpConfig
 
 
 class SummarizeViewSet(viewsets.ViewSet):
-    def create(self, request, *args, **kwargs):
+    def update(self, request, *args, **kwargs):
         input_text = request.data.get("text")
         task_id = request.data.get("task_id")
+
+        try:
+            task = Task.objects.get(id=task_id)
+
+        except ObjectDoesNotExist:
+            return Response({"error": "Task not found"}, status=status.HTTP_404_NOT_FOUND)
+
         generated_text = self.perform_summary(input_text)
-        Task.objects.get(task_id=task_id).delete()
-        task_data = {"task": task_id, "summarized_text": generated_text}
-        task_serializer = SubTaskSerializer(data=task_data)
+        task_data = {
+            "summarized_text": generated_text,
+        }
+        task_serializer = TaskSerializer(task, data=task_data, partial=True)
+
         if task_serializer.is_valid():
             task_serializer.save()
+
         else:
             return Response(task_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        return Response(task_serializer, status=status.HTTP_201_CREATED)
+
+        return Response(task_serializer.data, status=status.HTTP_201_CREATED)
 
     @staticmethod
     def perform_summary(input_text):
