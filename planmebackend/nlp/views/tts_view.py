a54cba840767ed4e57db3261fbdfeceaae75d883
Krittin SETDHAVANICH <krittin.set@ku.th>
2023-11-05 13:29:11 +0700

    split the job in ci.yml

--- planmebackend/nlp/views/tts_view.py
+++ planmebackend/nlp/views/tts_view.py
@@ -1,3 +1,4 @@
+import torch
 from rest_framework import status, viewsets
 from rest_framework.response import Response
 
@@ -50,8 +51,8 @@ class TTSViewSet(viewsets.ViewSet):
         model.eval()
         input_tensor = tokenizer.encode(input_text, return_tensors="pt").to("cpu")
 
-        # with torch.no_grad():
-        output = model.generate(input_tensor, max_length=1024)
+        with torch.no_grad():
+            output = model.generate(input_tensor, max_length=1024)
 
         decoded_output = tokenizer.decode(output[0], skip_special_tokens=True)
         decoded_output = decoded_output.replace(" and ", ", ").strip()
