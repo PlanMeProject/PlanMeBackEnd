bf2f99a1e13c890620799f7e82281e9dd6a15cf3
Krittin SETDHAVANICH <krittin.set@ku.th>
2023-11-06 15:28:58 +0700

    added views

---
+++ planmebackend/app/views/__init__.py
@@ -0,0 +1,4 @@
+"""A module that defines the ViewSet classes."""
+from planmebackend.app.views.SubTaskViewSet import SubTaskViewSet  # noqa: F401
+from planmebackend.app.views.TaskViewSet import TaskViewSet  # noqa: F401
+from planmebackend.app.views.UserViewSet import UserViewSet  # noqa: F401
