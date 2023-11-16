ba8bcbc5ceb9033f826f13fd12847934f0c4dd05
Krittin SETDHAVANICH <krittin.set@ku.th>
2023-11-16 16:28:57 +0700

    updated init

--- planmebackend/classroom/views/__init__.py
+++ planmebackend/classroom/views/__init__.py
@@ -1,4 +1,3 @@
 from planmebackend.classroom.views.AssignmentsViewSet import AssignmentsViewSet  # noqa: F401
 from planmebackend.classroom.views.AuthorizationViewSet import AuthorizationViewSet  # noqa: F401
 from planmebackend.classroom.views.CoursesViewSet import CoursesViewSet  # noqa: F401
-from planmebackend.classroom.views.google_classroom_api import GoogleClassroomAPI  # noqa: F401
