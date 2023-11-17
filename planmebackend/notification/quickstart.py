fbf49319faf7ca36fe3327b589fa2ddb88c58c98
Phumrapee Chaowanapricha <phumrapee.ch@ku.th>
2023-11-17 19:43:01 +0700

    Remove unused method.

--- planmebackend/notification/quickstart.py
+++ planmebackend/notification/quickstart.py
@@ -68,75 +68,6 @@ class GoogleClassroomAPI:
         user_info = self.user_info_service.userinfo().get().execute()
         return user_info.get('email')
 
-    def send_email(self, recipient_email, subject, body):
-        """
-        Sends an email to the specified recipient.
-        """
-        sender_email = "planmeproject.app@gmail.com"
-        password = "gopc cpyi lzhi blnx"
-
-        # Create MIME object
-        msg = MIMEMultipart()
-        msg['From'] = sender_email
-        msg['To'] = recipient_email
-        msg['Subject'] = subject
-        msg.attach(MIMEText(body, 'plain'))
-
-        # Setup the SMTP server
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.starttls()
-        server.login(sender_email, password)
-
-        # Send the email and close the connection
-        server.send_message(msg)
-        server.quit()
-
-    def load_seen_assignments(self):
-        """Load seen assignments from a file."""
-        try:
-            with open('assignments_seen.json', 'r', encoding='utf-8') as file:
-                return json.load(file)
-        except FileNotFoundError:
-            return {}
-
-    def save_seen_assignments(self):
-        """Save seen assignments to a file."""
-        with open('assignments_seen.json', 'w', encoding='utf-8') as file:
-            json.dump(self.assignments_seen, file)
-
-    def check_for_new_assignments(self):
-        """
-        Modified method to check for new assignments and send email notifications.
-        """
-        today = datetime.today().date()
-        recipient_email = self.get_user_email()
-        courses = self.get_courses()
-
-        for course in courses:
-            course_data, assignments_info = self.course_information(course)
-
-            for assignment in assignments_info:
-                assignment_id = assignment['course_id'] + "_" + assignment['title']
-                if assignment['due_date']:
-                    due_date = datetime.strptime(assignment['due_date'], '%Y-%m-%d').date()
-                else:
-                    continue
-
-                # Check if this is the first time seeing the assignment
-                if assignment_id not in self.assignments_seen:
-                    self.assignments_seen[assignment_id] = today.isoformat()
-                    email_subject = f"New Assignment in {course_data['course_name']}"
-                    email_body = f"New assignment: {assignment['title']} due in {assignment['due_date']}"
-                    self.send_email(recipient_email, email_subject, email_body)
-
-                # Check if today is three days before the due date
-                elif due_date - today == timedelta(days=3):
-                    email_subject = f"Upcoming Assignment Due in {course_data['course_name']}"
-                    email_body = f"{assignment['title']} is due in 3 days"
-                    self.send_email(recipient_email, email_subject, email_body)
-
-        self.save_seen_assignments()
-
     def get_courses(self):
         """
         Retrieves a list of courses where the student is enrolled.
@@ -278,4 +209,3 @@ class GoogleClassroomAPI:
 
 if __name__ == "__main__":
     classroom_api = GoogleClassroomAPI()
-    classroom_api.check_for_new_assignments()
