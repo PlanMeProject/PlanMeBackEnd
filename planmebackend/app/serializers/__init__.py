50b17a2cdc4e18a3202085c20c6cfadd5ceb8bde
Krittin SETDHAVANICH <krittin.set@ku.th>
2023-11-18 19:53:48 +0700

    added module docsting in app serializer

--- planmebackend/app/serializers/__init__.py
+++ planmebackend/app/serializers/__init__.py
@@ -1,3 +1,4 @@
+"""Import all serializers."""
 from planmebackend.app.serializers.deleted_task_serializer import DeletedTaskSerializer  # noqa: F401
 from planmebackend.app.serializers.subtask_serializer import SubTaskSerializer  # noqa: F401
 from planmebackend.app.serializers.task_serializer import TaskSerializer  # noqa: F401
