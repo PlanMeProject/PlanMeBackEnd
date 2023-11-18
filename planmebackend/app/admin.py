95b082c00073af3d651601f21890603f07775045
Krittin SETDHAVANICH <krittin.set@ku.th>
2023-11-18 14:23:03 +0700

    register DeletedTask in admin

--- planmebackend/app/admin.py
+++ planmebackend/app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import SubTask, Task, User
+from .models import DeletedTask, SubTask, Task, User
 
 admin.site.register(User)
 admin.site.register(Task)
 admin.site.register(SubTask)
+admin.site.register(DeletedTask)
