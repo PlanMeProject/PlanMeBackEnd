0014ad3287d2ace4f7fb7ea00225c331b238104e
Krittin SETDHAVANICH <krittin.set@ku.th>
2023-11-21 12:49:35 +0700

    modify production.yml use only django

--- config/settings/test.py
+++ config/settings/test.py
@@ -6,7 +6,7 @@ from .base import *  # noqa
 from .base import env
 
 # GENERAL
-# ------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env(
     "DJANGO_SECRET_KEY",
@@ -15,23 +15,23 @@ SECRET_KEY = env(
 # https://docs.djangoproject.com/en/dev/ref/settings/#test-runner
 TEST_RUNNER = "django.test.runner.DiscoverRunner"
 
-# PASSWORDS
-# ------------------------------------------------------------------------------
-# https://docs.djangoproject.com/en/dev/ref/settings/#password-hashers
-PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
+# # PASSWORDS
+# # ---------------------------------------------------------------------------
+# # https://docs.djangoproject.com/en/dev/ref/settings/#password-hashers
+# PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
 
-# EMAIL
-# ------------------------------------------------------------------------------
-# https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
-EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
-
-# DEBUGGING FOR TEMPLATES
-# ------------------------------------------------------------------------------
-TEMPLATES[0]["OPTIONS"]["debug"] = True  # type: ignore # noqa: F405
-
-# MEDIA
-# ------------------------------------------------------------------------------
-# https://docs.djangoproject.com/en/dev/ref/settings/#media-url
-MEDIA_URL = "http://media.testserver"
-# Your stuff...
-# ------------------------------------------------------------------------------
+# # EMAIL
+# # ---------------------------------------------------------------------------
+# # https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
+# EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
+#
+# # DEBUGGING FOR TEMPLATES
+# # ---------------------------------------------------------------------------
+# TEMPLATES[0]["OPTIONS"]["debug"] = True  # type: ignore # noqa: F405
+#
+# # MEDIA
+# # ---------------------------------------------------------------------------
+# # https://docs.djangoproject.com/en/dev/ref/settings/#media-url
+# MEDIA_URL = "http://media.testserver"
+# # Your stuff...
+# # ---------------------------------------------------------------------------
